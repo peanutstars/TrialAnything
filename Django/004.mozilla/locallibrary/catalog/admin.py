b2c27593ed3e92b9602091e9851afe5e531be410
HSLee <peanutstars.lee@gmail.com>
2018-11-26 13:57:59 +0900

    Tutorial Part4.

--- Django/004.mozilla/locallibrary/catalog/admin.py
+++ Django/004.mozilla/locallibrary/catalog/admin.py
@@ -1,3 +1,39 @@
 from django.contrib import admin
+from catalog.models import Author, Genre, Book, BookInstance
 
-# Register your models here.
+
+# admin.site.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+
+admin.site.register(Author, AuthorAdmin)
+
+
+class BookInstanceInline(admin.TabularInline):
+    model = BookInstance
+
+
+# admin.site.register(Book)
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'display_genre')
+    inlines = [BookInstanceInline]
+
+
+# admin.site.register(BookInstance)
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    # list_display = (BookInstance, 'status', 'due_back')
+    list_filter = ('status', 'due_back')
+    fieldsets = (
+        (None, {
+            'fields': ('book', 'imprint', 'id')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back')
+        }),
+    )
+
+
+admin.site.register(Genre)
--- Django/004.mozilla/locallibrary/catalog/models.py
+++ Django/004.mozilla/locallibrary/catalog/models.py
@@ -29,6 +29,12 @@ class Book(models.Model):
     def get_absolute_url(self):
         return reverse('book-detail', args=[str(self.id)])
 
+    def display_genre(self):
+        '''Create a string for the Genre. This is a required to display genre in Admin.'''
+        return ', '.join(genre.name for genre in self.genre.all()[:3])
+
+    display_genre.short_description = 'Genre'
+
 
 class BookInstance(models.Model):
     '''Model representing a specific copy of a book (i.e. that can be borrowed from the library).'''
