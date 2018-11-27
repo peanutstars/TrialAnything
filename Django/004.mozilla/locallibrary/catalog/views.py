8d07f3342d1164ee5202ab8089b268129eb956a0
HSLee <peanutstars.lee@gmail.com>
2018-11-27 18:37:16 +0900

    Tutorial Part 6.

--- Django/004.mozilla/locallibrary/catalog/views.py
+++ Django/004.mozilla/locallibrary/catalog/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
+from django.views import generic
 
 # Create your views here.
 from catalog.models import Book, Author, BookInstance, Genre
 
+
+class UserData(dict):
+    def __init__(self, *args, **kwargs):
+        super(UserData, self).__init__(*args, **kwargs)
+        self['a'] = 'aaaa'
+        self['b'] = 2
 
 def index(request):
     '''View function for home page of site.'''
@@ -19,5 +26,18 @@ def index(request):
         'num_instances_available': num_instances_available,
         'num_authors': num_authors,
         'num_genre': num_genre,
+        'data': UserData(),
     }
     return render(request, 'index.html', context=context)
+
+
+class BookListView(generic.ListView):
+    model = Book
+    paginate_by = 1
+
+class BookDetailView(generic.DetailView):
+    model = Book
+
+
+class AuthorListView(generic.ListView):
+    model = Author
